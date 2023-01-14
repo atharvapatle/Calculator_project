81620d2eb96b5a9921abf2bc9388ff0fc24bd2fc
Atharva patle <atharvapatle001@gmail.com>
2023-01-14 10:11:42 +0530

    ukukiu

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">12,14,129</div>
-          <div className="current-operand">0 454545</div>
+        <div className="previous-operand"></div>
+          <div className="current-operand"></div>
           </div>
           <button className="span-two">AC</button>        
           <button>DEL</button>
